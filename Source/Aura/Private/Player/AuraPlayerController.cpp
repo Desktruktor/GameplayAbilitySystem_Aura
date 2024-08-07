eba326c4336435a27fe3589177965bc30857a91e
Desktruktor <big.dreams.studio@gmail.com>
2024-08-06 20:01:53 -0600

    Explicacion de configuracion para el Enhanced Input

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
@@ -11,4 +12,24 @@ AAuraPlayerController::AAuraPlayerController()
 void AAuraPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
+
+	// Implementacion de Aura Context
+	check(AuraContext);		//checamos si existe este contexto
+	
+	// Esta clase permite gestionar y configurar mappings de entrada y contextos de entrada de manera más dinámica y eficiente
+	//-->El ULocalPlayer Representa a un jugador en el contexto de una sesión local (es decir, un jugador que está usando el juego en una sola máquina y no a través de red)
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()); 
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0); //Se lo agregamos al Contexto de AuraContext
+
+	
+	bShowMouseCursor = true; // Mostramos Mouse en pantalla
+	DefaultMouseCursor = EMouseCursor::Default; // Utilizamos el Mous por defecto
+
+	
+	// Configurar el modo de entrada que permite tanto la interacción con la UI como el control del jugador.
+	FInputModeGameAndUI InputModeData;		
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
 }
--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -6,6 +6,8 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
+
+class UInputMappingContext;
 /**
  * 
  */
@@ -18,4 +20,10 @@ public:
 	AAuraPlayerController();
 protected:
 	virtual void BeginPlay() override;
+
+private:
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> AuraContext;	
+	
 };
