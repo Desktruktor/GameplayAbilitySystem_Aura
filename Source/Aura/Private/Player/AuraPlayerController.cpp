0a8e76cf33fcf3ebfe92295696faba659f84d12a
Desktruktor <big.dreams.studio@gmail.com>
2024-08-06 23:10:26 -0600

    Se realizo la implementacion del Movement Input

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -3,6 +3,7 @@
 
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
@@ -17,7 +18,7 @@ void AAuraPlayerController::BeginPlay()
 	check(AuraContext);		//checamos si existe este contexto
 	
 	// Esta clase permite gestionar y configurar mappings de entrada y contextos de entrada de manera más dinámica y eficiente
-	//-->El ULocalPlayer Representa a un jugador en el contexto de una sesión local (es decir, un jugador que está usando el juego en una sola máquina y no a través de red)
+	// El ULocalPlayer Representa a un jugador en el contexto de una sesión local (es decir, un jugador que está usando el juego en una sola máquina y no a través de red)
 	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()); 
 	check(Subsystem);
 	Subsystem->AddMappingContext(AuraContext, 0); //Se lo agregamos al Contexto de AuraContext
@@ -32,4 +33,30 @@ void AAuraPlayerController::BeginPlay()
 	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
 	InputModeData.SetHideCursorDuringCapture(false);
 	SetInputMode(InputModeData);
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2d InputAxisVector = InputActionValue.Get<FVector2d>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation = FRotator(0.f, Rotation.Yaw, 0.f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	if (APawn* ControlledPawn = GetPawn<APawn>())	
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+
+	
 }
